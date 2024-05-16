351c88d2b1026aa10092198ab1ad6709490d2d08
gshin02 <gshin02@uw.edu>
2024-05-15 18:40:07 -0700

    major update :O

--- public/app.js
+++ public/app.js
@@ -1,77 +1,99 @@
 (function() {
   window.addEventListener("load", init);
+ 
+  // const FILE_NAMES = ["person1.png", "person2.jpg", "person3.png", "person4.jpg",
+  // "person5.png", "person6.png", "pet1.png", "pet2.png", "pet3.png"]
 
+  const FILE_NAMES = ["person1.png", "person3.png", "person5.png", "person6.png",
+                      "pet1.png", "pet2.png", "pet3.png"]
+ 
+ 
+  let clickedTime;
+  let createdTime;
+  let reactionTime;
+ 
+ 
+  let currTrial = 1;
+  let numTrials = 3;
+  let total = 0.0;
+ 
+ 
+ 
+ 
   function init() {
-    makeBox();
+    let button = document.getElementById('start');
+    button.addEventListener('click', start);
   }
-
-  function getRandomColor() {
-				
-    var letters = "0123456789ABCDEF".split('');
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.round(Math.random() * 15)];
-    } //ends for loop 
-    return color; 
-  
-  
-  } // ends getRandomColor Function
-  
-  
-  var clickedTime; var createdTime; var reactionTime; 
-  
-  function makeBox() {
-      var time=Math.random();
-      time=time*3000;
-    
-    setTimeout(function() {
-    
-      if (Math.random()>0.5) {
-      
-        document.getElementById("box").style.borderRadius="100px";
-        
-        } else {
-        
-          document.getElementById("box").style.borderRadius="0";
+ 
+ 
+  function start() {
+    var video = document.getElementById("video");
+    video.play();
+    if (currTrial < numTrials) {
+      makeObstacle();
+    }
+ 
+ 
+    document.addEventListener("keydown", function(event) {
+      if (currTrial < numTrials) {
+        if (event.key === 'Enter') {
+          clickedTime = Date.now();
+          reactionTime = (clickedTime - createdTime) / 1000;
+          document.getElementById("box").innerHTML = "";
+          if (currTrial != numTrials) {
+            document.getElementById("curr-trial").innerHTML = "Trial " + (currTrial + 1);
+          }
+ 
+ 
+          currTrial += 1;
+          total += reactionTime;
+          makeObstacle();
         }
-        
-      var top= Math.random();
-        top= top*300;
-      var left= Math.random();
-        left= left*500; 
-        
-      document.getElementById("box").style.top = top + "px";
-      document.getElementById("box").style.left = left + "px"; 
-    
-      document.getElementById("box").style.backgroundColor=getRandomColor();
-    
-      document.getElementById("box").style.display="block";
-      
-      createdTime=Date.now();
-      
-    }, time); 
-  
+      } else {
+        let avgReactionTime = total / numTrials;
+        document.getElementById("print-avg").innerHTML = "Your Average Reaction Time: "
+                                                          + avgReactionTime.toFixed(2) + " seconds";
+        stop();
+      }
+    });
+ 
+ 
   }
-  
-  document.getElementById("box").onclick=function() {
-  
-    clickedTime=Date.now();
-    
-    reactionTime=(clickedTime-createdTime)/1000;
-    
-    document.getElementById("printReactionTime").innerHTML="Your Reaction Time is: " + reactionTime + "seconds";
-    
-    this.style.display="none";
-    
-    makeBox();
-    
-    
+ 
+ 
+  function stop() {
+    var video = document.getElementById("video");
+    video.stop();
   }
-})
+ 
+ 
+  function makeObstacle() {
+    var time = Math.random() * 3000;
+ 
+ 
+    setTimeout(function() {
+      document.getElementById("box").innerHTML = "";
+ 
+ 
+      let obstacle = document.createElement("img");
+      var index = Math.floor(Math.random() * FILE_NAMES.length);
+      obstacle.src = "img/" + FILE_NAMES[index];
+ 
+ 
+      obstacle.alt = "obstacle";
+      obstacle.style.width = "100%";
+ 
 
+      var box = document.getElementById("box");
+      box.appendChild(obstacle);
 
+      var top = Math.random() * 300;
+      var left = Math.random() * 500;
 
-
-
-
-
+      box.style.top = top + "px";
+      box.style.left = left + "px";
+      box.style.display = "block";
+      createdTime = Date.now();
+    }, time);
+  }
+ })();

938885d6f682f4ed5d3a411e0c558aaeeb23b8f5
gshin02 <gshin02@uw.edu>
2024-05-16 10:03:12 -0700

    the off-by-one error should be gone now :DDDD + obstacle's position is more consistent now, still have to change the key inputs

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 (function() {
   window.addEventListener("load", init);
 
-  // const FILE_NAMES = ["person1.png", "person2.jpg", "person3.png", "person4.jpg",
-  // "person5.png", "person6.png", "pet1.png", "pet2.png", "pet3.png"]
-
   const FILE_NAMES = ["person1.png", "person3.png", "person5.png", "person6.png",
                       "pet1.png", "pet2.png", "pet3.png"]
 
@@ -13,50 +10,51 @@
   let createdTime;
   let reactionTime;
 
-  let currTrial = 0;
+  let currTrial = 1;
   let numTrials = 3;
   let total = 0.0;
+
   let obstacleDisplayed = true;
+
 
   function init() {
     let button = document.getElementById('start');
     button.addEventListener('click', start);
   }
 
+
   function start() {
     var video = document.getElementById("video");
     video.play();
 
-    if (currTrial < numTrials) {
-      if (currTrial == 0) {
-        makeObstacle();
-      }
-      document.addEventListener("keydown", enterPressed);
+    console.log(currTrial)
+    if (currTrial <= numTrials) {
+      document.addEventListener("keydown", keyIsPressed);
+      makeObstacle();
     } else {
       let avgReactionTime = total / numTrials;
       id("print-avg").innerHTML = "Your Average Reaction Time: " +
                                    avgReactionTime.toFixed(2) + " seconds";
       id("box").innerHTML = "";
-      stop();
+
       obstacleDisplayed = false;
-      document.removeEventListener("keydown", enterPressed);
+      document.removeEventListener("keydown", keyIsPressed);
+
+      stop();
     }
   }
 
 
-  function enterPressed(event) {
-    if (currTrial >= numTrials) {
+  function keyIsPressed(event) {
+    if (currTrial > numTrials) {
       obstacleDisplayed = false;
     }
+
     if (event.key === 'Enter' && (obstacleDisplayed)) {
       clickedTime = Date.now();
       reactionTime = (clickedTime - createdTime) / 1000;
-      currTrial += 1;
       total += reactionTime;
-      makeObstacle();
-
       id("box").innerHTML = "";
-      id("curr-trial").innerHTML = "Trial " + currTrial;
     }
   }
 
@@ -67,52 +65,49 @@
 
 
   function makeObstacle() {
+    id("curr-trial").innerHTML = "Trial " + (currTrial);
+    currTrial += 1;
+
     var time = Math.random() * 5000;
 
     setTimeout(function() {
-      document.getElementById("box").innerHTML = "";
+      id("box").innerHTML = "";
 
       let obstacle = document.createElement("img");
-      var index = Math.floor(Math.random() * FILE_NAMES.length);
+      let index = Math.floor(Math.random() * FILE_NAMES.length);
       obstacle.src = "img/" + FILE_NAMES[index];
       obstacle.alt = "obstacle";
       obstacle.style.width = "80%";
 
-      var box = document.getElementById("box");
-
-      var viewportWidth = document.body.offsetWidth;
-      var viewportHeight = document.body.offsetHeight;
-
-      var boxWidth = box.offsetWidth;
-      var boxHeight = box.offsetHeight;
-
-      var maxLeft = viewportWidth - boxWidth;
-      var maxTop = viewportHeight - boxHeight;
-
-      var left = Math.random() * maxLeft;
-      var top = Math.random() * maxTop;
-
-      if (left < 0) {
-        left = 0;
-      }
-
-      if (top < 0) {
-        top = 0;
-      }
-
-      if (top < viewportHeight / 2 && left < viewportWidth / 2) {
-        top += viewportHeight / 2;
-        left += viewportWidth / 2;
-      }
-
-      box.style.top = top + "px";
-      box.style.left = left + "px";
-
-      box.style.display = "block";
+      let box = id("box");
       box.appendChild(obstacle);
+      positionObstacle()
       createdTime = Date.now();
 
     }, time);
+  }
+
+
+  function positionObstacle() {
+    let maxLeft = 800;
+    let maxTop = 600;
+    let randomPosition = Math.floor(Math.random() * 3);
+    let left, top;
+
+    if (randomPosition === 0) {
+      left = 0;
+      top = maxTop / 2;
+    } else if (randomPosition === 1) {
+      left = (maxLeft - 0) / 2;
+      top = maxTop / 2;
+    } else {
+      left = maxLeft;
+      top = maxTop / 2;
+    }
+
+    id("box").style.left = left + "px";
+    id("box").style.top = top + "px";
+    id("box").style.display = "block";
   }
 
   function id(name) {
